eecab5e1ef35b0ef29e50b55d2a5c0c42412c41f
Ron Y <31330116+i-ron-y@users.noreply.github.com>
2017-12-02 02:37:45 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Ubyssey Latest ISSUU Extractor
+
+Extracts the URL and cover image of the latest Ubyssey print issue from [The Ubyssey's ISSUU page](https://issuu.com/ubyssey).
+
+
+## Usage
+
+Install Python.
+
+Install the required packages:
+
+````
+pip install selenium
+pip install BeautifulSoup4
+````
+
+Download and unzip [geckodriver](https://github.com/mozilla/geckodriver/releases), and add it to PATH.
+
+Go to the directory where `ubyssey-latest-issuu-extractor.py` is located.
+
+Run:
+
+````
+python ubyssey-latest-issuu-extractor.py
+````
---
+++ ubyssey-latest-issuu-extractor.py
@@ -0,0 +1,22 @@
+from contextlib import closing
+from selenium.webdriver import Firefox
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+
+url = 'https://issuu.com/ubyssey/'
+
+with closing(Firefox()) as browser:
+	browser.get(url)
+	WebDriverWait(browser, timeout=10).until(
+		lambda x: x.find_elements_by_class_name('cover'))
+	page_source = browser.page_source
+
+soup = BeautifulSoup(page_source, 'html.parser')
+
+latestIssue = soup.findAll('a', {'class': 'cover'})[0]
+
+issueUrl = 'https://issuu.com' + latestIssue.get('href')
+print(issueUrl)
+
+coverImage = latestIssue.find('img').get('src')
+print(coverImage)
